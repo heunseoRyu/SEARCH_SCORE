edad2c84def686d5d693e33e93a636791286d18e
heunseoRyu <heunseo9@gmail.com>
2024-12-24 11:47:33 +0900

    권한 설정

--- src/main/java/com/example/gal/domain/members/config/WebSecurityConfig.java
+++ src/main/java/com/example/gal/domain/members/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig {
                         .requestMatchers(HttpMethod.GET,"/files/{id}/remove").hasAnyAuthority("ADMIN","TEACHER")
                         .requestMatchers(HttpMethod.GET,"files/search").hasAnyAuthority("ADMIN","TEACHER")
                         .requestMatchers(HttpMethod.POST, "/files/{id}").hasAnyAuthority("ADMIN","TEACHER") // POST 요청 /files/{id}는 ADMIN 권한 필요
-                        .requestMatchers(HttpMethod.GET, "/files/{id}/scores/{key}").hasAnyAuthority("ADMIN","TEACHER") // PATCH 요청 /files/{id}/scores/{key}는 STUDENT 권한 필요
+                        .requestMatchers(HttpMethod.GET, "/files/{id}/scores/{key}").authenticated() // PATCH 요청 /files/{id}/scores/{key}는 STUDENT 권한 필요
                         .requestMatchers(HttpMethod.GET, "/files/{id}","files/upload","/users","/members/{id}/init-pw","members/{id}/delete","/members/{id}/allow").hasAnyAuthority("ADMIN","TEACHER") // GET 요청 /files/{id}는 ADMIN 권한 필요
                         .requestMatchers("/all").hasAuthority("ADMIN")
                         .anyRequest().authenticated())
